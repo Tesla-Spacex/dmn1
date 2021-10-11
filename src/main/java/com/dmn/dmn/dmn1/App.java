03df27efe3d21dfd2574b62932ea10daf744f98d
Slim <Slim@Admin>
2021-10-11 13:41:03 +0530

    dmn push

---
+++ src/main/java/com/dmn/dmn/dmn1/App.java
@@ -0,0 +1,13 @@
+package com.dmn.dmn.dmn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
